1bd87ae63498ca0329a2b17a930a036d59ddf407
747624 <747624@123-PC25.student.Tor.Loc>
2019-05-14 16:47:11 -0400

    go

--- src/intro/testscores.java
+++ src/intro/testscores.java
@@ -1,10 +1,25 @@
 package intro;
 
+import java.util.Scanner;
+
 public class testscores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    Scanner Keyboard = new Scanner(System.in);
+     System.out.println("enter test 1");
+     double sc1=Keyboard.nextDouble();
+     
+     System.out.println("enter test 1");
+     
+     double sc2=Keyboard.nextDouble();
+     
+     
+     System.out.println("enter test 1");
+		
+     double sc3=Keyboard.nextDouble();
+     double avg=(sc1+sc2+sc3)/3 ;
+     System.out.println(avg);
 	}
 
 }
