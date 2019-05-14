b36ea242339666619091ee66c6343bfb59fbf91a
747624 <747624@123-PC25.student.Tor.Loc>
2019-05-14 16:29:24 -0400

    intial commit

---
+++ src/intro/landconverter.java
@@ -0,0 +1,17 @@
+package intro;
+
+import java.util.Scanner;
+
+public class landconverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("how many squarefeet?");
+      Scanner keyboard =new Scanner(System.in);
+      double squarefeet = keyboard.nextInt();
+      
+      double acres =squarefeet/43560;
+      System.out.println("number of acres: " + acres);
+	}
+
+}
