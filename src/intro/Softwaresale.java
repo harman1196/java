a29605705951f253e078386199942bc6f8246903
747624 <747624@123-PC43.student.Tor.Loc>
2019-05-21 14:51:21 -0400

    sof

---
+++ src/intro/Softwaresale.java
@@ -0,0 +1,37 @@
+package intro;
+
+import java.util.Scanner;
+
+public class Softwaresale {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner Keyboard = new Scanner(System.in);
+  System.out.println("how many sof u wnt to purchase");
+  int numSoftware =Keyboard.nextInt();
+  double discount=0;
+  if(numSoftware >=10 &&numSoftware<=19){
+	  discount=0.20;
+  }
+  else if (numSoftware>=20 &&numSoftware<=49){
+	  discount=0.30;
+  }
+  else if (numSoftware>=50 &&numSoftware<=99){
+	  discount=0.40;
+  }
+  else if (numSoftware>=100){
+	  discount=0.50;
+  }
+  double subtotal =99*numSoftware;
+  double discountAmount=subtotal*discount;
+  double finalAmount=subtotal-discountAmount;
+  
+  System.out.println("subtotal:$"+subtotal);
+  System.out.println("discountAmount:$"+discountAmount);
+  System.out.println("finalAmount:$"+finalAmount);
+  
+	}
+
+}
